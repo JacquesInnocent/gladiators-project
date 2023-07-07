f66c0339682b3c1b499c02500b3e390a7abd461d
Nyadru Innocent <jacquesinnocentadv@gmail.com>
2023-07-07 08:46:56 +0000

    Corrected birthyear variable contents

--- Challenge2.js
+++ Challenge2.js
@@ -1,10 +1,10 @@
 const birthYear = prompt("What's your year of birth?")
-if (birthYear <= 17){
+if (birthYear => 2006){
     alert('You are a minor. No alcohol for you')
 }
-    if (birthYear > 17 && birthYear == 35 ){
+    if (birthYear <= 2006 && birthYear >= 1988){
         alert('You are a youth. Welcome to the bar')
     }
-    else if(birthYear => 36){
+    else if(birthYear <= 1987){
         alert('You are an elder mate. Enjoy')
     }
